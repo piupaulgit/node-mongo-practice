a2305228c795ccbae077b0ec62a54953b88c8ad1
piupaulgitm <contactpiu619@gmail.com>
2022-02-20 23:05:14 +0530

    patch delete

--- routes/employees.js
+++ routes/employees.js
@@ -24,6 +24,29 @@ router.get('/:id', async(req,res) => {
     }
 })
 
+router.patch('/:id', async(req,res) => {
+    try{
+        const emp = await Employee.findById(req.params.id);
+        emp.skill = req.body.skill
+        const resp = emp.save()
+        res.json(resp)
+    }
+    catch(err){
+
+    }
+})
+
+router.delete('/:id', async(req,res) => {
+    try{
+        const emp = await Employee.findById(req.params.id);
+        const resp = emp.remove()
+        res.json(resp)
+    }
+    catch(err){
+
+    }
+})
+
 router.post('/', async(req,res) => {
     const employee = new Employee({
         name: req.body.name,
